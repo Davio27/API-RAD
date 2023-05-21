27db9d2abccccbadb824c5187d3bbce145f1dafc
Washington <washington.bezerra@creditas.com>
2023-05-20 22:16:20 -0300

    changed type of named currency

--- application/entities/CurrencyQuotationEntity.py
+++ application/entities/CurrencyQuotationEntity.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
-from application.model.enum.Currencies import Currencies
 from application.model.enum.QuotationType import QuotationType
 
 
 @dataclass
 class CurrencyQuotationEntity:
-    name: Currencies
+    name: str
     date: str
     quotationBR: str
     type: QuotationType
