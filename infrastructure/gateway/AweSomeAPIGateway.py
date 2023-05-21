0f250397ef7713348e1f867f481605593d20b77d
Washington <washington.bezerra@creditas.com>
2023-05-20 22:17:24 -0300

    new gateway

--- infrastructure/gateway/AweSomeAPIGateway.py
+++ infrastructure/gateway/AweSomeAPIGateway.py
@@ -1,5 +1,16 @@
 import requests
+from application.model.enum.Currencies import Currencies
 
 
-def get_quotation(currency, days):
+def get_quotation_gateway(currency, days):
     return requests.get(f"https://economia.awesomeapi.com.br/json/daily/{currency}-BRL/{days}")
+
+
+def get_quotation_one_day(currency: Currencies, year, month, day):
+    if month < 10:
+        month = '0' + str(month)
+    if day < 10:
+        day = '0' + str(day)
+
+    date = year + str(month) + str(day)
+    return requests.get(f"https://economia.awesomeapi.com.br/json/daily/{currency}-BRL/?start_date={date}&end_date={date}")
